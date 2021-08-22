11de64b7b53a87b4be58c8767301bee5954a02aa
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2021-08-22 20:59:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let score = 100;
+let score = 20;
 
 let highScore = 0;
-let magicNumber = Math.trunc(Math.random() * 1000) + 1;
+let magicNumber = Math.trunc(Math.random() * 20) + 1;
 
 const displayMessage = function (message)
 {
